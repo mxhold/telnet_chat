c306d2dd82173b93f7e570e9f31254cd52ec6802
Max Holder <mxhold@gmail.com>
2015-10-05 20:07:18 -0700

    extract client; add log envs

--- lib/telnet_chat.ex
+++ lib/telnet_chat.ex
@@ -99,7 +99,7 @@ defmodule TelnetChat do
       Logger.metadata(ip: ip(client))
       Logger.debug "connection open"
       try do
-        serve(client)
+        TelnetChat.Client.serve(client)
       catch
         :exit, "closed" -> Logger.debug "connection closed"
       end
@@ -108,100 +108,102 @@ defmodule TelnetChat do
     loop_acceptor(socket)
   end
 
-  # Telnet codes
-
-  @tn_ECHO <<1>>
-  @tn_SUPPRESS_GO_AHEAD <<3>>
-  @tn_LINEMODE <<34>>
-
-  @tn_WILL <<251>>
-  @tn_WONT <<252>>
-  @tn_DO <<253>>
-  @tn_DONT <<254>>
-
-  @tn_IAC <<255>>
-
-  defp serve(socket) do
-    :gen_tcp.send(socket, "Username: ")
-    {:ok, response} = :gen_tcp.recv(socket, 0)
-
-    if response == @tn_IAC <> @tn_DO <> @tn_SUPPRESS_GO_AHEAD do
-      {:ok, response} = :gen_tcp.recv(socket, 0)
-    end
-
-    name = ignore_telnet_stuff(response) |> String.strip
-
-    TelnetChat.Server.join(TelnetChat.ChatServer, name)
-    Logger.info "#{name} joined."
-
-    turn_on_character_mode(socket)
-
-    :gen_tcp.send(socket, "> ")
-
-    serve(socket, name)
-  end
-
-  def ignore_telnet_stuff(response) do
-    case response do
-      @tn_IAC <> <<_, _>> <> rest -> rest
-      msg -> msg
-    end
-  end
-
-  defp turn_on_character_mode(socket) do
-    :gen_tcp.send(socket, @tn_IAC <> @tn_DONT <> @tn_ECHO)
-    :gen_tcp.send(socket, @tn_IAC <> @tn_WILL <> @tn_ECHO)
-    :gen_tcp.send(socket, @tn_IAC <> @tn_WILL <> @tn_SUPPRESS_GO_AHEAD)
-    :gen_tcp.send(socket, @tn_IAC <> @tn_DONT <> @tn_LINEMODE)
-    :gen_tcp.send(socket, @tn_IAC <> @tn_WONT <> @tn_LINEMODE)
-  end
-
-  defp clear(length) do
-    "\r#{String.duplicate(" ", length)}"
-  end
-
-  defp send_and_reprint_buffer(socket, buffer, message) do
-    :gen_tcp.send(socket, "#{clear(String.length(buffer) + 3)}\r#{message}> #{buffer}")
-  end
-
-  defp serve(socket, name, buffer \\ "") do
-    receive do
-      {:join, name}         -> send_and_reprint_buffer(socket, buffer, "#{name} joined.\r\n")
-      {:say, name, message} -> send_and_reprint_buffer(socket, buffer, "#{name}: #{message}\r\n")
-      {:part, name}         -> send_and_reprint_buffer(socket, buffer, "#{name} left.\r\n")
-      _ -> nil
-    after 0 -> nil
-    end
-
-    case :gen_tcp.recv(socket, 1, 0) do
-      {:ok, "\r"} ->
-        Logger.info "#{name}: #{buffer}"
-        TelnetChat.Server.say(TelnetChat.ChatServer, buffer)
-        buffer = ""
-      {:ok, "\d"} -> # backspace
-        buffer = String.slice(buffer, 0..-2)
-        send_and_reprint_buffer(socket, buffer, "")
-      {:ok, <<27>>} -> # ignore next 2 bytes after escape
-        {:ok, ignored_bytes} = :gen_tcp.recv(socket, 2)
-        Logger.warn "Ignored escaped bytes: #{inspect ignored_bytes}"
-      {:ok, char} ->
-        if char > <<31>> && char < <<127>> do
-          Logger.debug inspect(char)
-          :gen_tcp.send(socket, char)
-          buffer = buffer <> char
-        end
-      {:error, :timeout} -> nil
-      {:error, :closed} ->
-        Logger.info "#{name} left."
-        TelnetChat.Server.part(TelnetChat.ChatServer)
-        exit("closed")
-    end
-
-    serve(socket, name, buffer |> ignore_telnet_stuff)
-  end
-
-  def ip(socket) do
+  defp ip(socket) do
     {:ok, {ip, _}} = :inet.peername(socket)
     ip |> Tuple.to_list |> Enum.join(".")
+  end
+
+  defmodule Client do
+    # Telnet codes
+
+    @tn_ECHO <<1>>
+    @tn_SUPPRESS_GO_AHEAD <<3>>
+    @tn_LINEMODE <<34>>
+
+    @tn_WILL <<251>>
+    @tn_WONT <<252>>
+    @tn_DO <<253>>
+    @tn_DONT <<254>>
+
+    @tn_IAC <<255>>
+
+    def serve(socket) do
+      :gen_tcp.send(socket, "Username: ")
+      {:ok, response} = :gen_tcp.recv(socket, 0)
+
+      if response == @tn_IAC <> @tn_DO <> @tn_SUPPRESS_GO_AHEAD do
+        {:ok, response} = :gen_tcp.recv(socket, 0)
+      end
+
+      name = ignore_telnet_stuff(response) |> String.strip
+
+      TelnetChat.Server.join(TelnetChat.ChatServer, name)
+      Logger.info "#{name} joined."
+
+      turn_on_character_mode(socket)
+
+      :gen_tcp.send(socket, "> ")
+
+      serve(socket, name)
+    end
+
+    def ignore_telnet_stuff(response) do
+      case response do
+        @tn_IAC <> <<_, _>> <> rest -> rest
+        msg -> msg
+      end
+    end
+
+    defp turn_on_character_mode(socket) do
+      :gen_tcp.send(socket, @tn_IAC <> @tn_DONT <> @tn_ECHO)
+      :gen_tcp.send(socket, @tn_IAC <> @tn_WILL <> @tn_ECHO)
+      :gen_tcp.send(socket, @tn_IAC <> @tn_WILL <> @tn_SUPPRESS_GO_AHEAD)
+      :gen_tcp.send(socket, @tn_IAC <> @tn_DONT <> @tn_LINEMODE)
+      :gen_tcp.send(socket, @tn_IAC <> @tn_WONT <> @tn_LINEMODE)
+    end
+
+    defp clear(length) do
+      "\r#{String.duplicate(" ", length)}"
+    end
+
+    defp send_and_reprint_buffer(socket, buffer, message) do
+      :gen_tcp.send(socket, "#{clear(String.length(buffer) + 3)}\r#{message}> #{buffer}")
+    end
+
+    defp serve(socket, name, buffer \\ "") do
+      receive do
+        {:join, name}         -> send_and_reprint_buffer(socket, buffer, "#{name} joined.\r\n")
+        {:say, name, message} -> send_and_reprint_buffer(socket, buffer, "#{name}: #{message}\r\n")
+        {:part, name}         -> send_and_reprint_buffer(socket, buffer, "#{name} left.\r\n")
+        _ -> nil
+      after 0 -> nil
+      end
+
+      case :gen_tcp.recv(socket, 1, 0) do
+        {:ok, "\r"} ->
+          Logger.info "#{name}: #{buffer}"
+          TelnetChat.Server.say(TelnetChat.ChatServer, buffer)
+          buffer = ""
+        {:ok, "\d"} -> # backspace
+          buffer = String.slice(buffer, 0..-2)
+          send_and_reprint_buffer(socket, buffer, "")
+        {:ok, "\e"} -> # ignore next 2 bytes after escape
+          {:ok, ignored_bytes} = :gen_tcp.recv(socket, 2)
+          Logger.debug "Ignored escaped bytes: #{inspect ignored_bytes}"
+        {:ok, char} ->
+          if char > <<31>> && char < <<127>> do
+            Logger.debug inspect(char)
+            :gen_tcp.send(socket, char)
+            buffer = buffer <> char
+          end
+          {:error, :timeout} -> nil
+        {:error, :closed} ->
+          Logger.info "#{name} left."
+          TelnetChat.Server.part(TelnetChat.ChatServer)
+          exit("closed")
+      end
+
+      serve(socket, name, buffer |> ignore_telnet_stuff)
+    end
   end
 end
